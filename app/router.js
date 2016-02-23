b2e87f2f0cbd9de56d269773567a2656c769c72f
ayaz15m <ayaz15m@yahoo.com>
2016-02-23 16:57:40 -0600

    router

--- app/router.js
+++ app/router.js
@@ -1,11 +1,12 @@
-import Ember from 'ember';
+import Ember from 'ember';  
 import config from './config/environment';
 
-const Router = Ember.Router.extend({
+var Router = Ember.Router.extend({  
   location: config.locationType
 });
 
-Router.map(function() {
+Router.map(function() {  
+  this.route('posts', {path: '/posts/:post_id'});
 });
 
-export default Router;
+export default Router;  
