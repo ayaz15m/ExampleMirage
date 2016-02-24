4a45274c0a4bc338b4fcbe9928bb1c2a7fe5dded
ayaz15m <ayaz15m@yahoo.com>
2016-02-24 14:04:58 -0600

    removed user factory, not needed

--- app/mirage/factories/user.js
+++
@@ -1,6 +0,0 @@
-import Mirage, {faker} from 'ember-cli-mirage';
-
-export default Mirage.Factory.extend({  
-  'first-name'() { return faker.name.firstName(); },
-  'last-name'() { return faker.name.lastName(); }
-});
